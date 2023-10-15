19dcb071e35cbcbcc27c148c10b127c8385ecdc9
Aditya Kumar <adityakumar.bt21ele@pec.edu.in>
2023-10-15 12:29:26 +0530

    Initial Login SignUp Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Home from './components/Home';
+import LoginSignUp from './components/LoginSignUp';
 import Navbar from './components/Navbar';
 import Update from './components/Update';
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Update/>
-
+    <LoginSignUp/>
     </>
   );
 }
