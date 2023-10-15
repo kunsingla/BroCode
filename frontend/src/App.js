681937ca391868b6bd55a746c129bcbd94e8289f
Mayank <myank07official@gmail.com>
2023-10-15 11:15:32 +0530

    updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Update from './components/Update';
 
 function App() {
   return (
-    <></>
+    <>
+    <Navbar/>
+    <Update/>
+
+    </>
   );
 }
 
